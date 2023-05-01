d7a21d9467949811bf131d4dd1796ff3486d13a3
ArtemUb <savenkoam@gmail.com>
2023-05-01 14:19:35 +0900

    task23 is Done

---
+++ task23/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+void buildIntoCube(int num)
+{
+    int count = 1;
+    while (num != 0)
+    {        
+        Console.Write($"{Math.Pow(count, 3)} ");
+        count++;
+        num--;
+    }
+    Console.WriteLine("");
+}
+
+try
+{
+    int num = Prompt("Enter a number:");
+    buildIntoCube(num);
+}
+catch
+{
+    Console.WriteLine("Be careful, Enter an integer!");
+}
